cd69692c9de0a29b1d0e70a492206b1ea396fcb5
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-05-04 14:12:32 -0400

    Update

--- StringEqualityTivaR/StringEqualityForm.cs
+++ StringEqualityTivaR/StringEqualityForm.cs
@@ -12,7 +12,8 @@ using System.Windows.Forms;
  * Created on: 02-05-2018
  * Created for: ICS3U Programming
  * Daily Assignment – Day #33 - String Equality
- * This program...
+ * This program allows the user to enter two numbers and then compares...
+ * ... them in uppercase and lowercase to see if they are the same.
 */
 
 namespace StringEqualityTivaR
@@ -30,14 +31,30 @@ namespace StringEqualityTivaR
         // Procedure: StringsAreEqual
         // Input: stringOne and stringTwo
         // Output: bool isEqual
-        // Description: 
+        // Description: It checks to see if the strings are equal in length...
+        // ... and when they are both capitalized
         private bool StringsAreEqual ()
         {
             // Declare local Variables
             bool isEqual = false;
             string upcaseWordOne, upcaseWordTwo;
 
-            // 
+            // To find their length
+            if (wordOne.Length == wordTwo.Length )
+            {
+                // Convert the strings to upper case and then compare them
+                upcaseWordOne = wordOne.ToUpper();
+                upcaseWordTwo = wordTwo.ToUpper();
+
+                // See if they are the same still
+                if (upcaseWordOne == upcaseWordTwo)
+                {
+                    // If they are the same set the boolean to true
+                    isEqual = true;
+                }
+
+            }
+
 
             return isEqual;
         }
